c14a4b28d9cb865a97bb473aa717102630e644dd
unknown <harshbajani7@gmail.com>
2024-04-02 12:33:22 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider';
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Boom',
+  description: 'Video Conferencing App',
+  icons:'/icons/logo.svg'
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from '@/components/ui/toaster';
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import 'react-datepicker/dist/react-datepicker.css'
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Boom',
+  description: 'Video Conferencing App',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <ClerkProvider
+        appearance={{
+          layout: {
+            socialButtonsVariant: 'iconButton',
+            logoImageUrl: '/icons/boom-logo1.svg',
+          },
+          variables: {
+            colorText: '#fff',
+            colorPrimary: '#0E78F9',
+            colorBackground: '#1C1F2E',
+            colorInputBackground: '#252A41',
+            colorInputText: '#fff',
+          },
+        }}
+      >
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+          <Toaster />
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
